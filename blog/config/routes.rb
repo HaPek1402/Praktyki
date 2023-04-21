a7e8271cb6e5d9182efcccab36ab902bac135164
HaPek1402 <91899778+HaPek1402@users.noreply.github.com>
2023-04-21 10:43:35 +0200

    added admin panel

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -4,11 +4,23 @@ Rails.application.routes.draw do
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
+
   # Defines the root path route ("/")
   root "articles#index"
 
   resources :articles do
     resources :comments
   end
+  get '/admin', to: redirect('/admin/articles')
+  namespace :admin do
+    resources :articles do
+      resources :comments
+      get 'new', to: 'articles#new', as: 'new'
+    end
+    
+    get 'articles/:id', to: 'articles#show', as: 'admin_article'
+    get 'articles/:id/comments', to: 'comments#index', as: 'article_comments'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html  
 end
