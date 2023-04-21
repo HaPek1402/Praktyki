03a17bede04325e3bca843182b4b46dda5094056
HaPek1402 <91899778+HaPek1402@users.noreply.github.com>
2023-04-21 11:50:47 +0200

    added pdf_generation

--- blog/app/controllers/admin/articles_controller.rb
+++ blog/app/controllers/admin/articles_controller.rb
@@ -33,7 +33,32 @@ class Admin::ArticlesController < ArticlesController
     @article.destroy
     redirect_to admin_articles_path, status: :see_other
   end
+  
+  def generate_pdf
+    article = Article.find(params[:id])
+    comments = article.comments.order(:created_at)
 
+    pdf = Prawn::Document.new
+    pdf.text article.title, size: 24, style: :bold
+    pdf.move_down 20
+    pdf.text article.body
+
+    pdf.move_down 20
+    pdf.text "Comments", size: 18, style: :bold
+    pdf.move_down 10
+
+    comments.each do |comment|
+      pdf.text "Comment by #{comment.commenter}", style: :italic
+      pdf.text comment.body
+      pdf.move_down 10
+    end
+
+    file_path = Rails.root.join('public', "#{article.title.parameterize}.pdf")
+    pdf.render_file file_path
+
+    redirect_to admin_article_path(article), notice: "PDF saved successfully!"
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :body, :status)
