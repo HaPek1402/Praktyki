2c5b7136235ffacf86f48c069d80469400419662
HaPek1402 <91899778+HaPek1402@users.noreply.github.com>
2023-04-26 14:56:08 +0200

    added page numbering

--- blog/app/controllers/admin/articles_controller.rb
+++ blog/app/controllers/admin/articles_controller.rb
@@ -35,11 +35,11 @@ class Admin::ArticlesController < ArticlesController
   end
 
   def generate_pdf
-    article = Article.find(params[:id])
-    comments = article.comments.order(:created_at)
-    file_path = Rails.root.join('public', "#{article.id}-#{article.title.parameterize}.pdf")
+    @article = Article.find(params[:id])
+    @comments = @article.comments.order(:created_at)
+    file_path = Rails.root.join('public', "#{@article.id}-#{@article.title.parameterize}.pdf")
     if File.exist?(file_path)
-      if (article.last_comment_deletion? && (article.last_comment_deletion > article.last_pdf_generated_at)) || (article.updated_at > article.last_pdf_generated_at) || (comments.present? && (comments.maximum(:updated_at) > article.last_pdf_generated_at))
+      if (@article.last_comment_deletion? && (@article.last_comment_deletion > @article.last_pdf_generated_at)) || (@article.updated_at > @article.last_pdf_generated_at) || (@comments.present? && (@comments.maximum(:updated_at) > @article.last_pdf_generated_at))
         FileUtils.rm(file_path)
       end
     end
@@ -47,25 +47,27 @@ class Admin::ArticlesController < ArticlesController
         # Download last pdf in browser
         send_file file_path, type: 'application/pdf', disposition: 'attachment'
     else
-      pdf = Prawn::Document.new
-      pdf.text article.title, size: 24, style: :bold
-      pdf.move_down 20
-      pdf.text article.body
-  
-      pdf.move_down 20
-      pdf.text "Comments", size: 18, style: :bold
-      pdf.move_down 10
-  
-      comments.each do |comment|
-        pdf.text "Comment by #{comment.commenter}", style: :italic
-        pdf.text comment.body
-        pdf.move_down 10
+      html = render_to_string(action: :show)
+      PDFKit.configure do |config|
+        config.default_options = {}
       end
+      PDFKit.configure do |config|
+        config.default_options = {
+          header_center: "My Blog",
+          header_font_size: 48,
+          footer_right: "Page [page] of [toPage]"
+        }
+      end
+      
+      pdf = PDFKit.new(html, page_size: 'Letter').to_pdf
+      File.open(file_path, 'wb') do |file|
+        file.write(pdf)
+      end
+      send_data(pdf, filename: "#{@article.id}-#{@article.title.parameterize}.pdf", type: "application/pdf")
       # Save new pdf and update last_pdf_generated_at
-      pdf.render_file file_path
-      article.update_column(:last_pdf_generated_at, Time.now)  
+      @article.update_column(:last_pdf_generated_at, Time.now)  
       # Download new pdf in browser
-      send_file file_path, type: 'application/pdf', disposition: 'attachment'
+      #send_data pdf, filename: 'file_name.pdf', type: 'application/pdf', disposition: 'attachment'
     end
     # Check if last pdf generated at is nil or if there were any changes
   end
