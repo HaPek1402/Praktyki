c3084e95309918530562a8957324a9d61e3d855b
HaPek1402 <91899778+HaPek1402@users.noreply.github.com>
2023-04-27 12:29:44 +0200

    added pagination

--- blog/app/controllers/admin/comments_controller.rb
+++ blog/app/controllers/admin/comments_controller.rb
@@ -1,9 +1,5 @@
 class Admin::CommentsController < CommentsController
     before_action :require_admin!
-    def index
-        @article = Article.find(params[:article_id])
-        @comments = @article.comments
-    end
     def destroy
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
