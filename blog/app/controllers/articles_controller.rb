39c918b666e2835a6badd06cc00767e6c0b12f52
HaPek1402 <91899778+HaPek1402@users.noreply.github.com>
2023-04-27 12:26:10 +0200

    added pagination

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @article = Article.find(params[:id])
+    @comments = @article.comments.paginate(page: params[:page], per_page: 10)
   end
 
   private
